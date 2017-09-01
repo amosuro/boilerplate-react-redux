9edfb28a5bb0a3e732b9d4a2140277ebac852d11
Ashley Mosuro <ashley@mosuro.co.uk>
2017-09-01 11:52:33 +0100

    [SEED] - Add tslint & lint script

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    public render() {
         return (
             <div>
                 <p>Hello world!</p>
             </div>
-        )
+        );
     }
 }
 
 ReactDOM.render(
     <App />,
-    document.getElementById('app')
+    document.getElementById('app'),
 );
