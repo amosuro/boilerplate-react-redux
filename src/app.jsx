0257ae7d122300fe36f0ba536650f7ee734585d1
Ashley Mosuro <ashley@mosuro.co.uk>
2017-09-01 18:09:18 +0100

    [SEED] - add ESLint (tslint replacement)

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    public render() {
+    render() {
         return (
             <div>
                 <p>Hello world!</p>
